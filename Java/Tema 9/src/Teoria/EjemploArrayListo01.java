c0f96fa7362ee4d08b31cc2273c51c236c30d824
danielsevillasn <daniel.sevilla.sn@gmail.com>
2026-03-11 11:46:40 +0100

    Renombrar carpeta tema 9

---
+++ Java/Tema 9/src/Teoria/EjemploArrayListo01.java
@@ -0,0 +1,25 @@
+package Teoria;
+
+import java.util.ArrayList;
+
+public class EjemploArrayListo01 {
+    public static void main(String[] args) {
+        ArrayList<String> a = new ArrayList<String>();
+
+        System.out.println("Nª de elementos: "+ a.size());
+
+        a.add("rojo");
+        a.add("verde");
+        a.add("azul");
+
+        System.out.println("Nª de elementos: "+ a.size());
+
+        a.add("blanco");
+
+        System.out.println("Nª de elementos: "+ a.size());
+
+        System.out.println("El elemento que hay en la posicion 0 es "+a.get(0));
+        System.out.println("El elemento que hay en la posicion 3 es "+a.get(3));
+
+    }
+}
